7ee744d1518d52015ddec1394acf461aa315fe55
Lacie <dark830917@gmail.com>
2019-11-20 15:48:58 +0800

    串接 送答案 API

--- chaibidon/Controller/ChaiViewController.swift
+++ chaibidon/Controller/ChaiViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ChaiViewController: UIViewController {
 
     var roomData: RoomData?
+    var user_a_option: Int?
+    var user_b_option: Int? = 1
     
     let activityIndicatorView = UIActivityIndicatorView(style: .large)
     var myScore: Int?
@@ -38,14 +40,35 @@ class ChaiViewController: UIViewController {
     
     @IBAction func pressScissors(_ sender: UIButton) {
         mySelectImage.image = UIImage(named: "scissors")
+        user_a_option = 1
+        user_b_option = 1
+        
+        if user_b_option == 1 {
+            otherSelectImage.image = UIImage(named: "scissors")
+        }
+        sendSelection()
     }
     
     @IBAction func pressRock(_ sender: UIButton) {
         mySelectImage.image = UIImage(named: "rock")
+        user_a_option = 2
+        user_b_option = 2
+        
+        if user_b_option == 2 {
+            otherSelectImage.image = UIImage(named: "rock")
+        }
+        sendSelection()
     }
     
     @IBAction func pressPaper(_ sender: UIButton) {
         mySelectImage.image = UIImage(named: "paper")
+        user_a_option = 3
+        user_b_option = 3
+        
+        if user_b_option == 3 {
+            otherSelectImage.image = UIImage(named: "paper")
+        }
+        sendSelection()
     }
     
 
@@ -85,9 +108,39 @@ extension ChaiViewController {
         }
     }
     
-    func sendYourSelection() {
+    func sendSelection() {
         
+        let passingData = GameOption(user_a_option: user_a_option!, user_b_option: user_b_option!)
+        
+        guard let uploadData = try? JSONEncoder().encode(passingData) else {
+            return
+        }
+        let url = URL(string: "https://85fb8eaa.ngrok.io/api/pss/send/14")!
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "Post"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
+
+            if let error = error {
+                print ("error: \(error)")
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode) else {
+                    print ("server error")
+                    return
+            }
+
+            if let mimeType = response.mimeType,
+                mimeType == "application/json",
+                let data = data,
+                let dataString = String(data: data, encoding: .utf8) {
+                print (dataString)
+            }
+        }
+        task.resume()
     }
-    
-    
 }
--- chaibidon/Model/ChaiStruct.swift
+++ chaibidon/Model/ChaiStruct.swift
@@ -8,6 +8,25 @@
 
 import Foundation
 
+// 基礎格式
+struct Foundation: Codable {
+    
+    let id: Int?
+    let round_id: Int?
+    let user_a_id: String?
+    let user_a_name: String?
+    let user_a_option: String?
+    let user_a_score: Int?
+    let user_b_id: String?
+    let user_b_name: String?
+    let user_b_option: String?
+    let user_b_score: Int?
+    let end: Int?
+    let created_at: String?
+    let updated_at: String?
+    
+}
+
 //以下登入用
 struct YourName: Codable {
     
@@ -16,60 +35,31 @@ struct YourName: Codable {
 }
 
 //以下接收登入後回傳資訊
-struct RoomFoundation: Codable {
-    
-    let id: Int?
-    let round_id: Int?
-    let user_a_id: String?
-    let user_a_name: String?
-    let user_a_option: String?
-    let user_a_score: Int?
-    let user_b_id: String?
-    let user_b_name: String?
-    let user_b_option: String?
-    let user_b_score: Int?
-    let end: Int?
-    let created_at: String?
-    let updated_at: String?
-    
-}
-
 struct LoginAndReceiveData: Codable {
     
-    let room: RoomFoundation
+    let room: Foundation
     let status: String
     
 }
 
-
 //以下監聽用
-struct Records: Codable {
-    
-    let id: Int?
-    let round_id: Int?
-    let user_a_id: String?
-    let user_a_name: String?
-    let user_a_option: String?
-    let user_a_score: Int?
-    let user_b_id: String?
-    let user_b_name: String?
-    let user_b_option: String?
-    let user_b_score: Int?
-    let end: Int?
-    let created_at: String?
-    let updated_at: String?
-    
-}
-
 struct DataFoundation: Codable {
     
     let game_name: String
-    let records: Records
+    let records: Foundation
     
 }
 
 struct RoomData: Codable {
     
     let data: DataFoundation
+    
+}
+
+//以下送剪刀石頭布答案
+struct GameOption: Codable {
+    
+    let user_a_option: Int
+    let user_b_option: Int
     
 }
