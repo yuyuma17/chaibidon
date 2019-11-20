d487a96d2b3539a3199c1e0838d76616e027599c
ABlackTaco <ablacktaco@gmail.com>
2019-11-20 14:44:27 +0800

    chaibidon

--- chaibidon/Controller/BiViewController.swift
+++ chaibidon/Controller/BiViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class BiViewController: UIViewController {
 
     var initialTime: Int?
+    var countDownTimer: Timer?
+    var answerPoint: Int = 0
+    var repeatToGetResultTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,36 +22,102 @@ class BiViewController: UIViewController {
     }
     
     @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var myAnswerImage: UIImageView!
+    @IBOutlet var enemyAnswerImage: UIImageView!
     
+    @IBOutlet var drawButton: UIButton!
     @IBAction func drawCard(_ sender: UIButton) {
+        countDownTimer?.invalidate()
+        timerLabel.text = "等待對方抽卡..."
         
+        answerPoint = Int.random(in: 1...13)
         
+        sender.isEnabled = false
+        sender.backgroundColor = .darkGray
+        
+        myAnswerImage.image = UIImage(named: "\(answerPoint)")
+        postRandomPoint()
+        repeatToGetResult()
     }
-    
 
 }
 
-
 extension BiViewController {
-    
-    
-    
     
     func startTimer() {
         
         initialTime = 15
         
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+        countDownTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
             self.timerLabel.text = "剩餘時間： \(self.initialTime!) 秒"
             if self.initialTime != 0 {
                 self.initialTime! -= 1
             } else {
                 timer.invalidate()
-                //call API
+                self.postRandomPoint()
+                self.repeatToGetResult()
             }
         }
         
     }
     
+    func postRandomPoint() {
+        let answer = BiAnswer(name: "123我", answer: answerPoint)
+        guard let uploadData = try? JSONEncoder().encode(answer) else { return }
+                
+        let url = URL(string: "https://c9aa79d8.ngrok.io/api/vs/send")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+        let task = URLSession.shared.uploadTask(with: request, from: uploadData) { (data, response, error) in
+            if let error = error {
+                print ("error: \(error)")
+                return
+            }
+            if let response = response as? HTTPURLResponse {
+                print("status code: \(response.statusCode)")
+                if let mimeType = response.mimeType,
+                    mimeType == "application/json",
+                    let data = data,
+                    let dataString = String(data: data, encoding: .utf8) {
+                    print ("got data: \(dataString)")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func repeatToGetResult() {
+        repeatToGetResultTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { (timer) in
+            self.getResult()
+        }
+    }
+    
+    func getResult() {
+        let address = "https://c9aa79d8.ngrok.io/api/vs/watch"
+        if let url = URL(string: address) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("error: \(error.localizedDescription)")
+                    return
+                }
+                guard let data = data else { return }
+                if let response = response as? HTTPURLResponse {
+                    print("status code: \(response.statusCode)")
+                    if let biResult = try? JSONDecoder().decode(BiResult.self, from: data) {
+                        print(biResult)
+                        if biResult.data.winner != "Not yet" {
+                            self.repeatToGetResultTimer?.invalidate()
+                            DispatchQueue.main.async {
+                                self.drawButton.isEnabled = true
+                            }
+                            
+                        }
+                    }
+                }
+            }.resume()
+        }
+    }
     
 }
--- chaibidon/Model/APIStruct.swift
+++ chaibidon/Model/APIStruct.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct BiWait: Codable {
+    
+    var data: Int
+    
+}
+
+struct BiAnswer: Codable {
+    
+    var name: String
+    var answer: Int
+    
+}
+
+struct BiResult: Codable {
+    
+    var data: Data
+    
+    struct Data: Codable {
+        var a_answer: Int?
+        var b_answer: Int?
+        var winner: String
+    }
+    
+}
