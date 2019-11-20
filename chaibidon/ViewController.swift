f271d869207ae1ebf1be6ab0764df6da93be9971
Lacie <dark830917@gmail.com>
2019-11-20 10:48:22 +0800

    first commit

---
+++ chaibidon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chaibidon
+//
+//  Created by 黃士軒 on 2019/11/20.
+//  Copyright © 2019 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
